f84a89b0ab6869e23cee3725362903164216145e
juanmejia017 <juanmejia0712123@gmail.com>
2026-02-12 12:06:43 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        int age = 25;
+        double height = 1.75;
+        bool is_student = true;
+        string name = "Ana";
+        char grade = 'A';
+
+       // Imprimir variables
+        Console.WriteLine("La edad es: " + age);
+        Console.WriteLine("La estatura es: " + height);
+        Console.WriteLine("Es estudiante?: " + is_student);
+        Console.WriteLine("El nombre es: " + name);
+        Console.WriteLine("La calificación es: " + grade);
+    }
+}
